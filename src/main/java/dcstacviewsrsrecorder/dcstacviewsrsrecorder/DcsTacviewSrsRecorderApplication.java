7b53b64dca2113fd10516794bcee496882f8c969
Jim Vella <whoisjim@hotmail.com>
2021-05-23 20:39:31 +1000

    start.spring.io

---
+++ src/main/java/dcstacviewsrsrecorder/dcstacviewsrsrecorder/DcsTacviewSrsRecorderApplication.java
@@ -0,0 +1,13 @@
+package dcstacviewsrsrecorder.dcstacviewsrsrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcsTacviewSrsRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcsTacviewSrsRecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dcstacviewsrsrecorder/dcstacviewsrsrecorder/DcsTacviewSrsRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package dcstacviewsrsrecorder.dcstacviewsrsrecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcsTacviewSrsRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
